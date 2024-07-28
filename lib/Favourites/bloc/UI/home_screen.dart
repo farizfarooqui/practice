94343531808a0b2ed9bd2cc04a01e92d215a4752
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-07-28 19:59:15 +0500

    .

--- lib/Favourites/bloc/UI/home_screen.dart
+++ lib/Favourites/bloc/UI/home_screen.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
--- lib/Favourites/bloc/fav_bloc.dart
+++ lib/Favourites/bloc/fav_bloc.dart
@@ -1,7 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:practice/items.dart';
-
 part 'fav_event.dart';
 part 'fav_state.dart';
 
