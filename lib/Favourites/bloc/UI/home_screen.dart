084b0480f08e9673b63030d07b95a2527b154658
MUHAMMAD FARIZ FAROOQUI <135133746+farizfarooqui@users.noreply.github.com>
2024-08-03 22:52:57 +0500

    .

--- lib/Favourites/bloc/UI/home_screen.dart
+++ lib/Favourites/bloc/UI/home_screen.dart
@@ -5,11 +5,14 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
+
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Flutter Demo')),
+      appBar: AppBar(
+        title: const Text('Flutter Demo'),
+      ),
     );
   }
 }
